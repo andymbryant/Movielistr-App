29f89b90d72079667051ab071012d736045f01ce
Andy Bryant <a.mcdermid.bryant@gmail.com>
2018-01-04 15:34:11 -0800

    Added 200 test with Chai

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ var app = express();
 mongoose.connect('mongodb://heroku_n1ncd624:7ngrcj5fiib8c94e4dim03n8nj@ds229295.mlab.com:29295/heroku_n1ncd624')
 var db = mongoose.connection;
 
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var lists = require('./routes/lists');
---
+++ test/app-test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('App', function() {
+  describe('/', function() {
+    it('responds with status 200', function(done) {
+      chai.request(app)
+        .get('/')
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+});
